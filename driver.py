a2b078927524a1c56a11db8626774e8de3fe5d3f
justxiao <justxiao@umich.edu>
2022-08-10 12:46:51 -0400

    added end game option for every turn without needing to input extra

--- driver.py
+++ driver.py
@@ -30,30 +30,18 @@ def play():
     print()
 
     # sorted works since every list in solution should have exact same values and rows
-    check_sol_prompt = 0
     while sorted(solution) != sorted(curr_board):
         print()
         ss.print_board(curr_board)
         print()
 
-        # if user wants to quit, break out of loop; check every 5 moves
-        if check_sol_prompt % 5 == 0:
-            check_sol = input('Do you want to see the solution? WARNING: LOOKING AT THE SOLUTION \nWILL END THE GAME! [Y/N]: ')
-            while check_sol != 'N' and check_sol != 'Y':
-                print('Invalid input. Please enter "Y" or "N".')
-                check_sol = input('Do you want to see the solution? WARNING: LOOKING AT THE SOLUTION \nWILL END THE GAME![Y/N]: ')
-
-            if check_sol == 'Y':
-                print()
-                print('Better luck next time!')
-                break
-
         # store column and row values
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'board']
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'board', 'end']
         nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
         # get user input and check for invalid inputs
         print('Please enter "BOARD" to see the original board OR')
+        print('Enter "END" to see the solution OR')
         guess_col = input('Enter a column letter: ').lower()
         while guess_col not in letters:
             print()
@@ -69,6 +57,22 @@ def play():
             print('=' * 49)
             print()
             continue
+
+        # make sure they want to end game
+        if guess_col == 'end':
+            print()
+            check_sol = input('Do you want to see the solution? WARNING: LOOKING AT THE SOLUTION \nWILL END THE GAME! [Y/N]: ').lower()
+            while check_sol != 'n' and check_sol != 'y':
+                print('Invalid input. Please enter "Y" or "N".')
+                check_sol = input('Do you want to see the solution? WARNING: LOOKING AT THE SOLUTION \nWILL END THE GAME![Y/N]: ').lower()
+
+            if check_sol == 'y':
+                print()
+                print('Better luck next time!')
+                break
+            else:
+                print()
+                continue
         
         guess_row = input('Enter a row number: ')
         while guess_row not in nums:
@@ -87,24 +91,21 @@ def play():
         
         # update curr_board which is separate from solution and board
         curr_board[nums.index(guess_row)][letters.index(guess_col)] = int(input_num)
-        check_sol_prompt += 1
         if sorted(solution) == sorted(curr_board):
             print('Congrats! You win!')
 
     ss.print_board(solution)
     print()
-    play_again = input('Would you like to play again? [Y/N]: ')
-    while play_again != 'Y' and play_again != 'N':
-        print('Invalid input. Please enter "Y" or "N".')
-        play_again = input('Would you like to play again? [Y/N]: ')
-    if play_again == 'Y':
-        play()
-    else:
-        print()
 
 if __name__ == '__main__':
     print('Welcome to Sudoku!')
     print()
-    play()
+    play_again = 'y'
+    while play_again == 'y':
+        play()
+        play_again = input('Would you like to play again? [Y/N]: ').lower()
+        while play_again != 'y' and play_again != 'n':
+            print('Invalid input. Please enter "Y" or "N".')
+            play_again = input('Would you like to play again? [Y/N]: ').lower()
     print('Thanks for playing Sudoku!')
     print()
